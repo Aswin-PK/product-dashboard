67351bca3d647685c62e96aa7cc7e13aa29bdc6d
Aswin-PK <aswinpk7737@gmail.com>
2024-04-14 18:23:05 +0530

    feat: fetch all products and fetching based on product category

---
+++ src/store.js
@@ -0,0 +1,76 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    state: {
+        products: [],
+        categories: [],
+        tableColumnHeaders: [],
+        selectedCategory: '',
+        totalItems: null,
+        limit: 10,
+    },
+    mutations: {
+        SET_PRODUCTS(state, productItems) {
+            state.products = productItems
+        },
+        SET_CATEGORIES(state, categories) {
+            state.categories = categories
+        },
+        SET_TABLE_COLUMN_HEADERS(state) {
+            const headers = Object.keys(state.products[0])
+            state.tableColumnHeaders = headers
+        },
+        SET_TOTAL(state, total) {
+            state.totalItems = total
+        },
+    },
+    actions: {
+        async setProducts({commit, state}, {category=state.selectedCategory, skip=0}) {
+            state.selectedCategory = category
+            const skipCount = (state.selectedCategory == '') ? ( (skip == 0) ? 0 : (skip-1)*state.limit ) : 0
+
+            try {
+                const response = await axios.get(`https://dummyjson.com/products${state.selectedCategory ? '/category/' + state.selectedCategory : ''}?limit=${state.limit}&skip=${skipCount}`);
+                commit('SET_TOTAL', response.data.total)
+                commit('SET_PRODUCTS', response.data.products)
+                this.dispatch('setTableColumnHeaders')
+            }
+            catch(error) {
+                console.log("Something went wrong...", error)
+            }
+        },
+        async setCategories({commit}) {
+            try {
+                const response = await axios.get('https://dummyjson.com/products/categories');
+                commit('SET_CATEGORIES', response.data)
+            }
+            catch(error) {
+                console.log("Something went wrong...", error)
+            }
+        },
+
+        setTableColumnHeaders({commit}) {
+            commit('SET_TABLE_COLUMN_HEADERS')
+        },
+
+        
+    },
+    getters: {
+        getAllProducts(state) {
+            return state.products;
+        },
+        getAllCategories(state) {
+            return state.categories;
+        },
+        getTableColumnHeaders(state) {
+            return state.tableColumnHeaders;
+        },
+        getDisplayLimit(state) {
+            return {limit: state.limit, total: state.totalItems};
+        },
+    }
+})
