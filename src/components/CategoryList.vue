f59222ae7f517ee51969bac71108b3ff9658d890
Aswin-PK <aswinpk7737@gmail.com>
2024-04-18 12:55:21 +0530

    feat: Category list page added

---
+++ src/components/CategoryList.vue
@@ -0,0 +1,49 @@
+<template>
+    <el-row style="width: 100%;" :gutter="20">
+        <el-col :span="8" v-for="(category, index) in categories" :key="index" >
+            <div class="category-item" @click="handleCategory(category)">
+                <span>{{ category }}</span>
+                <span>
+                    <i class="el-icon-d-arrow-right"></i>
+                </span>
+            </div>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+    name: 'CategoryList',
+    
+    computed: {
+        categories() {
+            return this.$store.getters.getAllCategories;
+        },
+    },
+    methods: {
+        handleCategory(category) {
+            this.$store.dispatch('fetchProducts', {category: [category]});
+            this.$router.push({path: `/categories/${category}`})
+        }
+    },
+}
+</script>
+
+<style scoped>
+.el-col {
+    margin-top: 1em;
+}
+.category-item {
+    height: 3.5em;
+    background-color: rgb(206, 218, 228);
+    padding: 1em;
+    border-radius: 1em;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    cursor: pointer;
+}
+.category-item:hover {
+    transform: scale(1.02);
+}
+</style>
---
+++ src/pages/category-list.vue
@@ -0,0 +1,24 @@
+<template>
+    <el-container>
+        <CategoryList :tableData="getCategories" />
+    </el-container>
+</template>
+
+<script>
+import CategoryList from '@/components/CategoryList.vue';
+
+export default {
+    name: 'CategoryListPage',
+    components: {
+        CategoryList
+    },
+    computed: {
+        getCategories() {
+            return this.$store.getters.getAllCategories;
+        }
+    },
+}
+</script>
+
+<style scoped>
+</style>
