b8ce5cef7c07df9b7123c4af7a12b78aa1098820
Aswin-PK <aswinpk7737@gmail.com>
2024-05-21 11:23:43 +0530

    change the configuration according to vue3

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const webpack = require('webpack');
+
+module.exports = {
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', 'vue')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {}
+        }
+      })
+  },
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'false',
+      })
+    ],
+  }
+}
